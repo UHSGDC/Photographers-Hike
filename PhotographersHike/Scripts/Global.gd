e8d79a50869ec1af8e4c6e416e0c712c14b618d9
0megq <nvatanshenas55@gmail.com>
2023-01-08 21:21:46 -0800

    Room switching player fix
    The player has their y velocity set to the jump velocity if the room they enter is above the previous room.
    
    I do this so the player doesn't jump into a room and then fall out of it right away

--- PhotographersHike/Scripts/Global.gd
+++ PhotographersHike/Scripts/Global.gd
@@ -8,6 +8,12 @@ onready var platforming_player := get_tree().current_scene.get_node("Platforming
 var room_pause: bool = false
 export var room_pause_time: float = 0.2
 
+enum {
+	UP
+	RIGHT
+	DOWN
+	LEFT
+}
 
 func change_room(room_position: Vector2, room_size: Vector2) -> void:
 	player_camera.current_room_center = room_position
--- PhotographersHike/Scripts/PlatformingPlayer.gd
+++ PhotographersHike/Scripts/PlatformingPlayer.gd
@@ -32,6 +32,16 @@ var jump_was_pressed: bool = false
 var remember_jump_length: float = 0.1
 
 
+# Room swapping
+
+enum Touching_Side {
+	BOTH,
+	HORIZONTAL,
+	VERTICAL,
+	NONE
+}
+
+
 # Delta is the time since physics_process was last called
 # I multiply things by delta so things move correctly no matter the frame rate
 func _physics_process(delta: float) -> void:
@@ -119,9 +129,61 @@ func _on_RoomDetector_area_entered(area: Area2D) -> void:
 	var collision_shape: CollisionShape2D = area.get_node("CollisionShape2D")
 	var size: Vector2 = collision_shape.shape.extents * 2
 	
+	# Gives the player extra upward velocity if it is entering a room above it
+	if Global.player_camera.current_room_size != Vector2.ZERO:
+		var side = check_room_edge(Global.player_camera.current_room_center, Global.player_camera.current_room_size, collision_shape.global_position, size)
+	
+		if side == Global.UP:
+			velocity.y = jump_velocity
+	
 	# Changes camera's current room and size. check PlayerCamera script for more info
 	Global.change_room(collision_shape.global_position, size)
+
+
+# Checks which edge of a touching b. If a and b are overlapping or not touching 
+# the function will push_error(). a is the previous room and b is the room being entered
+func check_room_edge(a_center: Vector2, a_size: Vector2, b_center: Vector2, b_size: Vector2) -> int:
 	
-
-
+	var relative_center: Vector2 = a_center - b_center
+	
+	var total_size: Vector2 = a_size + b_size
+	
+	var horizontal_overlap: int = total_size.x / 2 - abs(relative_center.x)
+	var vertical_overlap: int = total_size.y / 2 - abs(relative_center.y)
+	
+	var touching: int
+	
+	if horizontal_overlap > 0 and vertical_overlap > 0:
+		touching = Touching_Side.BOTH
+	elif horizontal_overlap > 0 and vertical_overlap == 0:
+		touching = Touching_Side.VERTICAL
+	elif horizontal_overlap == 0 and vertical_overlap > 0:
+		touching = Touching_Side.HORIZONTAL	
+	elif horizontal_overlap <= 0 and vertical_overlap <= 0:
+		touching = Touching_Side.NONE
+	else:
+		push_error("error calculating room edge")
+		
+	match touching:
+		Touching_Side.BOTH:
+			push_error("rooms overlapping")
+		Touching_Side.NONE:
+			push_error("player crossed two rooms that are not touching")
+		Touching_Side.VERTICAL:
+			if a_center.y < b_center.y: # up is negative y
+				return Global.DOWN
+			elif a_center.y > b_center.y:
+				return Global.UP
+			else:
+				push_error("rooms touching vertically, but at same y coordinate")
+		Touching_Side.HORIZONTAL:
+			if a_center.x < b_center.x:
+				return Global.RIGHT
+			elif a_center.x > b_center.x:
+				return Global.LEFT
+			else:
+				push_error("rooms touching horizontally, but at same x coordinate")
+				
+	# Fail safe
+	return Global.RIGHT
 	
