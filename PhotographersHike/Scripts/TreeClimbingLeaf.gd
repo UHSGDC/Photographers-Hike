6b0439d9141d761550b2a968b3673e783ef61bc9
crashdotcom <107324814+crashdotcom@users.noreply.github.com>
2023-02-14 21:47:49 -0800

    Create scene/temp leaf obj
    - created scene, added it to main scene
    - created leaf object for player to climb up tree on, collidable w/ player. proper texture/sizing tbd
    
    TO-DO:
    - give leaf movement (already half set up, but not tested)
    - randomize leaf location
    - add bear and acorns & related mechanics

---
+++ PhotographersHike/Scripts/TreeClimbingLeaf.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+#func physics_process(delta):
+	#var distance = Vector2.DOWN * 100
+	#translate(distance)
+#	pass
+
+
+#func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	#queue_free()
---
+++ PhotographersHike/Scripts/TreeClimbingMini.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+onready var leaf := preload("res://Scenes/TreeClimbingLeaf.tscn")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_VisibilityNotifier2D_screen_entered():
+	var instance = leaf.instance()
+	add_child(instance)
+	print("added child")
