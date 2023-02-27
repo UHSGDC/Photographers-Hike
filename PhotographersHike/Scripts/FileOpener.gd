bbce4936fb25b557d813b969c595130a385c3afc
primefanatic1 <122232487+primefanatic1@users.noreply.github.com>
2023-02-26 20:59:20 -0800

    JSON Reading
    -Created file 'dialog.json' for use in all dialog conversations
    -FileOpener.gd:
         -function 'load_file' renamed 'file_read'
         -function 'file_append' added, instead of overwriting file, it adds data to the end of it
         -function 'read_json' added in order to read JSON files
         -made some of the function parameters statically typed
    -Added a function in 'Text Engine.gd' to use the newly created file 'dialog.json' to print out text
    -created scene for an upside down triangle at the bottom of the textbox to indicate that the text has finished printing (like in Pokemon)
         -

--- PhotographersHike/Scripts/FileOpener.gd
+++ PhotographersHike/Scripts/FileOpener.gd
@@ -3,19 +3,17 @@ extends Node
 #Just opens a text file, nothing crazy. 
 #But it could be used to load save data / highscores
 
-func _ready():
-	print(load_file("res://test_textfile.tres"))
 
-
-
-func file_write(filepath, content) -> void:
+func file_write(filepath: String, content: String) -> void:
 	var file = File.new()
 	file.open(filepath, File.WRITE)
 	file.store_string(content)
 	file.close()
-	
 
-func load_file(filepath) -> String:
+func file_append(filepath: String, content: String) -> void:
+	file_write(filepath, file_read(filepath) + content)
+
+func file_read(filepath: String) -> String:
 	var file = File.new()
 	file.open(filepath, File.READ)
 	var content = file.get_as_text()
@@ -23,3 +21,8 @@ func load_file(filepath) -> String:
 	return content
 
 
+func read_json(file) -> Dictionary: 
+	#var contents: String = file_read(file)
+	#var json_data = JSON.parse(contents).get_result()
+	
+	return JSON.parse(file_read(file)).get_result()
--- PhotographersHike/Scripts/Text Engine.gd
+++ PhotographersHike/Scripts/Text Engine.gd
@@ -4,10 +4,15 @@ extends Node2D
 
 
 const accepted_keys: Array = [KEY_SPACE, KEY_ENTER]
+const dialog_json_filepath: String = "res://dialog.json" #don't have the file yet
+
+var next_indicator = load("res://Scenes//NextIcon.tscn")
 
 onready var screen_dimensions = get_viewport_rect().size
 
 onready var label_style := preload("res://Fonts/Text Engine.tres")
+
+
 
 var strlist
 
@@ -16,14 +21,16 @@ signal next
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	yield(text_dialog("Hello World (Use the 'space' or 'enter' keys, or just click the textbox)", 15, 0.1, "Test Subject"), "completed")
-	yield(text_dialog("This is a very basic text engine", 15, 0.1, "Test Subject"), "completed")
-	yield(text_dialog("I hope this is okay", 15, 0.1, "Test Subject"), "completed")
-	yield(text_dialog("Text files can be read", 15, 0.1, "Test Subject"), "completed")
-	yield(text_dialog("For example: res://test_textfile.tres contains the following text:", 15, 0.1, "Test Subject"), "completed")
-	yield(text_dialog($FileOpener.load_file('res://test_textfile.tres'), 10, 0.1, "Test Subject"), "completed")
-	
+	yield(text_dialog("Hello World (Use the 'space' or 'enter' keys, or just click the textbox)", 15, 0.05, "Test Subject"), "completed")
+	yield(text_dialog("An indicator arrow will appear at the bottom of the textbox... now!", 15, 0.05, "Test Subject"), "completed")
+	yield(text_dialog("Text can go really fast...", 15, 0.01, "Test Subject"), "completed")
+	yield(text_dialog("Or instantaneously...", 15, 0, "Test Subject"), "completed")
+	yield(text_dialog("Or VERY slowly...", 15, 0.25, "Test Subject"), "completed")
+	yield(text_dialog("Text files can be read", 15, 0.05, "Test Subject"), "completed")
+	yield(text_dialog("As well as JSON files", 15, 0.05, "Test Subject"), "completed")
+	for index in range(3):
+		yield(json_text_dialog("Test Subject", str(index), 15, 0.05), "completed")
+	yield(json_text_dialog("Test Subject", "Farewell", 15, 0.05), "completed")
 
 func wait(seconds: float):
 	#use it like this: yield(wait(1), "completed")
@@ -31,14 +38,9 @@ func wait(seconds: float):
 	yield(get_tree().create_timer(seconds), "timeout")
 
 
-
-func get_json(file, key: String): #unfinished
-	var contents: String = $FileOpener.load_file(file)
-	
-
-
 func text_dialog(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "Nobody", sound = null):
 	#'sound' parameter should be AudioStreamPlayer2D. Not implemented yet.	
+	#should make it so it sounds like Celeste, but I need sounds in order to test it out and implement this
 	$CharacterName.text = speaker_name
 	label_style.size = size
 	$CharacterName.show()
@@ -51,23 +53,22 @@ func text_dialog(string: String, size: int = label_style.size, delay: float = 0.
 			$TextOutput.text += character
 			yield(wait(delay), "completed")
 	
-
+	var NextIndicator = next_indicator.instance()
+	add_child(NextIndicator)
 	
 	yield(self, "next")
 	
+	NextIndicator.queue_free()
 	$CharacterName.hide()
 	$TextOutput.hide()
 	$CharacterName.text = ""
 	$TextOutput.text = ""
 	
-	print("Done!")
-	
-	
 	return
 
-
-
-
+func json_text_dialog(speaker_name: String = "", key = "", size: int = label_style.size, delay: float = 0.0, sound = null) -> void:
+	yield(text_dialog($FileOpener.read_json(dialog_json_filepath)[speaker_name][key], size, delay, speaker_name, sound), "completed")
+	return
 
 
 #func set_size(size: int):
@@ -86,6 +87,6 @@ func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		emit_signal("next")
 
-func _on_Area2D_input_event(viewport, event, shape_idx):
+func _on_Area2D_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton or (event is InputEventKey and event.scancode in accepted_keys):
 		emit_signal("next")
