16bbece9d09b040e12d070c9f86905d37da947b9
primefanatic1 <122232487+primefanatic1@users.noreply.github.com>
2023-04-07 14:58:43 -0700

    User Input
    -Added a way for users to input text
    -Added a response system where players can select a response
    -Added JSON reading for those functions (unfinished)
    -Added Answer Box and Input scenes

--- PhotographersHike/Scripts/Text Engine.gd
+++ PhotographersHike/Scripts/Text Engine.gd
@@ -2,9 +2,14 @@ extends Control
 #I know that the head node is a control node (not Node2D), but I didn't want to create issues by renaming the node
 
 
-
+const AnswerOptionScene = preload("res://Scenes//Answer Option.tscn")
 const accepted_keys: Array = [KEY_SPACE, KEY_ENTER]
 const dialog_json_filepath: String = "res://dialog.json" #don't have the file yet
+
+const OptionScale = Vector2(0.4, 0.2)
+
+
+
 
 var next_indicator = load("res://Scenes//NextIcon.tscn")
 
@@ -14,25 +19,35 @@ onready var label_style := preload("res://Fonts/Text Engine.tres")
 
 
 
+var answer: String
+
 var strlist
 
 signal next
+signal answered
 
 
 # Important thing to note: if you want the background to pause, use yield(func, "completed") like I do here
 func _ready():
 	#$TextOutput.max_lines_visible = 4
-	yield(text_dialog("Hello World (Use the 'space' or 'enter' keys, or just click the textbox)", 15, 0.05, "Test Subject"), "completed")
-	yield(text_dialog("An indicator arrow will appear at the bottom of the textbox... now!", 15, 0.05, "Test Subject"), "completed")
-	yield(text_dialog("Text can go really fast...", 15, 0.01, "Test Subject"), "completed")
-	yield(text_dialog("Or instantaneously...", 15, 0, "Test Subject"), "completed")
-	yield(text_dialog("Or VERY slowly...", 15, 0.25, "Test Subject"), "completed")
-	yield(text_dialog("Text files can be read", 15, 0.05, "Test Subject"), "completed")
-	yield(text_dialog("As well as JSON files", 15, 0.05, "Test Subject"), "completed")
-	for index in range(3):
-		yield(json_text_dialog("Test Subject", str(index), 15, 0.05), "completed")
-	yield(json_text_dialog("Test Subject", "Farewell", 15, 0.05), "completed")
-	yield(text_dialog("Line1\nLine2\nLine3\nLine4\nLine5\nLine6", 15, 0.05, "Tester"), "completed")
+	
+	var name: String = yield(input_dialog("What is your name?"), "completed")
+	
+	yield(text_dialog("Hi " + name +  ". (Use the 'space' or 'enter' keys, or just click the textbox)", 15, 0.05, "Test Subject"), "completed")
+	
+	if yield(question_dialog("Would you like to proceed?", 15, 0.05), "completed") == "Yes":
+	
+	
+		yield(text_dialog("An indicator arrow will appear at the bottom of the textbox... now!", 15, 0.05, "Test Subject"), "completed")
+		yield(text_dialog("Text can go really fast...", 15, 0.01, "Test Subject"), "completed")
+		yield(text_dialog("Or instantaneously...", 15, 0, "Test Subject"), "completed")
+		yield(text_dialog("Or VERY slowly...", 15, 0.25, "Test Subject"), "completed")
+		yield(text_dialog("Text files can be read", 15, 0.05, "Test Subject"), "completed")
+		yield(text_dialog("As well as JSON files", 15, 0.05, "Test Subject"), "completed")
+		for index in range(3):
+			yield(json_text_dialog("Test Subject", str(index), 15, 0.05), "completed")
+		yield(json_text_dialog("Test Subject", "Farewell", 15, 0.05), "completed")
+		yield(text_dialog("Line1\nLine2\nLine3\nLine4\nLine5\nLine6", 15, 0.05), "completed")
 
 func wait(seconds: float):
 	#use it like this: yield(wait(1), "completed")
@@ -40,18 +55,20 @@ func wait(seconds: float):
 	yield(get_tree().create_timer(seconds), "timeout")
 
 
-func appear():
-	pass
 
-func disappear():
-	pass
 
-func text_dialog(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "Nobody", sound = null):
-	#'sound' parameter should be AudioStreamPlayer2D. Not implemented yet.	
-	#should make it so it sounds like Celeste, but I need sounds in order to test it out and implement this
-	$CharacterName.text = speaker_name
-	label_style.size = size
-	$CharacterName.show()
+
+
+
+func text_output(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "", sound = null) -> void:
+	#does not return anything, just saves some typing
+	$Input.hide()
+	if speaker_name:
+		$CharacterName.text = speaker_name
+		label_style.size = size
+		$CharacterName.show()
+	else:
+		$CharacterName.hide()
 	$TextOutput.show()
 	if delay <= 0.0:
 		$TextOutput.text = string
@@ -60,6 +77,18 @@ func text_dialog(string: String, size: int = label_style.size, delay: float = 0.
 		for character in string:
 			$TextOutput.text += character
 			yield(wait(delay), "completed")
+
+func text_disappear():
+	$CharacterName.hide()
+	$TextOutput.hide()
+	$CharacterName.text = ""
+	$TextOutput.text = ""
+
+
+func text_dialog(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "", sound = null):
+	#'sound' parameter should be AudioStreamPlayer2D. Not implemented yet.	
+	#should make it so it sounds like Celeste, but I need sounds in order to test it out and implement this
+	yield(text_output(string, size, delay, speaker_name, sound), "completed")
 	
 	var NextIndicator = next_indicator.instance()
 	add_child(NextIndicator)
@@ -67,10 +96,7 @@ func text_dialog(string: String, size: int = label_style.size, delay: float = 0.
 	yield(self, "next")
 	
 	NextIndicator.queue_free()
-	$CharacterName.hide()
-	$TextOutput.hide()
-	$CharacterName.text = ""
-	$TextOutput.text = ""
+	text_disappear()
 	
 	return
 
@@ -79,21 +105,65 @@ func json_text_dialog(speaker_name: String = "", key = "", size: int = label_sty
 	return
 
 
-func question_dialog(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "Nobody", options: Array = ["Yes", "No"], sound = null):
-	pass
-	#I may need to create some buttons for response options
+func question_dialog(string: String, size: int = label_style.size, delay: float = 0.0, speaker_name: String = "", answer_options: Array = ["Yes", "No"], sound = null):
+	var answerOptionScenes: Array = []
+	var OptionCoords: Vector2 = Vector2(402, 199) #start pos
+	#no more than 4 dialog options supported
+	
+	$TextOutput.rect_size.x = 315
+	
+	#use the subprocess (is it called that...?)
+	yield(text_output(string, size, delay, speaker_name, sound), "completed")
+	
+	var optionBoxInstance
+	
+	for option in answer_options:
+		
+		optionBoxInstance = AnswerOptionScene.instance().init(option, OptionCoords, OptionScale)
+		add_child(optionBoxInstance)
+		
+		optionBoxInstance.rect_position = OptionCoords
+		OptionCoords.y += 24 #this value works the best for spacing
+
+
+		answerOptionScenes.append(optionBoxInstance)
+		optionBoxInstance.connect("clicked", self, "_on_Answer_Selected")
+	
+	yield(self, "answered")
+
+	$TextOutput.rect_size.x = 446
+	
+	for node in answerOptionScenes:
+		node.queue_free()
+	
+	text_disappear()
+	
+	return answer
+
+func json_question_dialog(speaker_name: String = "", key = "", size: int = label_style.size, delay: float = 0.0, sound = null) -> void:
+	return yield(question_dialog($FileOpener.read_json(dialog_json_filepath)[speaker_name][key], size, delay, speaker_name, sound), "completed")
+	
+func _on_Answer_Selected(option):
+	answer = option
+	emit_signal("answered")
+
+
+func input_dialog(question: String): #returns a value based on input
+	text_disappear()
+	$Input.show()
+	$Input.init(question)
+	
+	yield(self, "answered")
+	
+	return answer
 	
 
-func input_dialog():
-	pass
-
+func json_input_dialog(speaker_name: String = "", key = ""):
+	return yield(input_dialog($FileOpener.read_json(dialog_json_filepath)[speaker_name][key]), "completed")
 
 #func set_size(size: int):
 #	label_style.size = size
-#func increase_size(size: int):
-#	label_style.size += size
-#func decrease_size(size: int):
-#	label_style.size -= size
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
