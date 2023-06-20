39fd038ef59941bddac35895046af5bd7ecad686
0megq <nvatanshenas55@gmail.com>
2023-06-20 14:10:59 -0700

    Changed Answer Container to grow upwards
    Also fixed bug to make container buttons show proper text

--- PhotographersHike/Dialog/AnswerContainer.gd
+++ PhotographersHike/Dialog/AnswerContainer.gd
@@ -5,11 +5,11 @@ export var answer_button_scene: PackedScene
 signal answer_selected(text)
 
 
-
 func init(answer_text_array: PoolStringArray) -> void:
 	for text in answer_text_array:
 		var answer_button: Button = answer_button_scene.instance()
 		add_child(answer_button)
+		answer_button.text = text
 		answer_button.connect("pressed", self, "_on_answer_selected", [text])
 
 	
