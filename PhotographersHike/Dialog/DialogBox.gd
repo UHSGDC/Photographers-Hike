fdc770998892ab698263ae4b92db37c4728b7e9b
0megq <nvatanshenas55@gmail.com>
2023-06-25 10:03:44 -0700

    Created npc scene

--- PhotographersHike/Dialog/DialogBox.gd
+++ PhotographersHike/Dialog/DialogBox.gd
@@ -41,6 +41,7 @@ const text_delay_dictionary: Dictionary = {
 
 
 func _ready() -> void:
+	Global.dialog_box = self
 	dialog_data = JsonOpener.get_data(dialog_json_filepath)
 	$NextIcon.hide()
 
---
+++ PhotographersHike/Interactables/NPC.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+export(DialogBox.Levels) var level
+export var npc_name: String
+
+var current_dialogue: int = 0
+
+
+func _on_InteractZone_player_interacted() -> void:
+	Global.dialog_box.play_dialog(npc_name, level, current_dialogue)
+	
