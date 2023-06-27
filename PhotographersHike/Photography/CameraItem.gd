da942750ea65eee6f524a71c607da891a22b1f8f
0megq <nvatanshenas55@gmail.com>
2023-06-26 20:29:57 -0700

    Added basic implementation
    Still need to create album and maybe save the files to disk

--- PhotographersHike/Photography/CameraItem.gd
+++ PhotographersHike/Photography/CameraItem.gd
@@ -1,21 +1,33 @@
 extends Node
 
+signal close_picture
+
 onready var captured_image = $CanvasLayer/CapturedImage
 
+var screenshot_textures: Array
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("take_picture"):
+var can_take_picture: bool = true
+
+var center: Vector2
+
+var Rng: RandomNumberGenerator
+		
+
+func _ready() -> void:
+	center = captured_image.rect_position
+	Rng = RandomNumberGenerator.new()
+	Rng.randomize()
+		
+		
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("take_picture") and can_take_picture:
 		take_screenshot()
 
 
 func take_screenshot() -> void:
-	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
-	# Wait until the frame has finished before getting the texture.
 	yield(VisualServer, "frame_post_draw")
-	
 	# Retrieve the captured image.
 	var image = get_viewport().get_texture().get_data()
-	image.resize(480, 288, 1)
 
 	# Flip it on the y-axis (because it's flipped).
 	image.flip_y()
@@ -26,12 +38,33 @@ func take_screenshot() -> void:
 
 	# Set the texture to the captured image node.
 	captured_image.set_texture(texture)
+	randomize_image_rotation()
+	randomize_image_position()
+
+	screenshot_textures.append(texture)
 	
-	var dir: Directory = Directory.new()
-	if dir.open("user://screenshots"):
-		dir.make_dir("user://screenshots")	
-	
-	image.save_png("user://screenshots/screenshot.png")
+	can_take_picture = false
+	$AnimationPlayer.play("take_picture")
+	yield($AnimationPlayer, "animation_finished")
+	can_take_picture = true
 
 	
-	print("pictureasdasd")
+func randomize_image_rotation() -> void:
+	captured_image.rect_rotation = Rng.randf_range(-5, 5)
+	
+	
+func randomize_image_position() -> void:
+	captured_image.rect_position = center + Vector2(Rng.randf_range(-5, 5), Rng.randf_range(-5, 5))
+	
+	
+	
+#Add later
+
+#func save_photo() -> void:
+#	var dir: Directory = Directory.new()
+#	if dir.open("user://screenshots"):
+#		dir.make_dir("user://screenshots")	
+#
+#	image.save_png("user://screenshots/screenshot.png")
+
+	
