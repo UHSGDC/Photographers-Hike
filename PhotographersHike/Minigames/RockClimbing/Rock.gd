e8032641ff70abb13b58c0882349f1950a9b5623
0megq <nvatanshenas55@gmail.com>
2023-07-21 20:46:06 -0700

    Preparing for playtest

--- PhotographersHike/Minigames/RockClimbing/Rock.gd
+++ PhotographersHike/Minigames/RockClimbing/Rock.gd
@@ -3,5 +3,5 @@ extends Area2D
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready() -> void:
-	rng.randomize()
+	rng.randomize()      
 	$Sprite.frame = rng.randi_range(0, 3)
