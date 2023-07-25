59ed3d3357c82af85a92deba5f03afe47f82ae3f
0megq <nvatanshenas55@gmail.com>
2023-07-24 20:04:13 -0700

    Updated Settings menu

--- PhotographersHike/Menus/SettingsMenu.gd
+++ PhotographersHike/Menus/SettingsMenu.gd
@@ -1,14 +1,6 @@
 extends BaseMenu
 
 
-func _on_Full_Checkbox_pressed():
-	OS.window_fullscreen = !OS.window_fullscreen
-
-
-func _on_Border_Checkbox_pressed():
-	OS.window_borderless = !OS.window_borderless
-
-
 func _on_Back_pressed() -> void:
 	menus.change_menu(menus.previous_menu)
 
@@ -23,3 +15,11 @@ func _on_ShowDebugBox_toggled(button_pressed):
 
 func _on_AimWithMouseCheckbox_toggled(button_pressed):
 	Global.aim_with_mouse_rock_climbing = button_pressed
+
+
+func _on_FullscreenCheckbox_toggled(button_pressed):
+	OS.window_fullscreen = button_pressed
+
+
+func _on_BorderlessBox_toggled(button_pressed):
+	OS.window_borderless = button_pressed
