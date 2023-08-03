154b1233fdaef4dea3e7b6954c37cc72edb221f4
0megq <nvatanshenas55@gmail.com>
2023-08-03 15:49:19 -0700

    Deleted old settings stuff

--- PhotographersHike/Menus/Settings/SettingsMenu.gd
+++ PhotographersHike/Menus/Settings/SettingsMenu.gd
@@ -1,50 +1,16 @@
 extends BaseMenu
 
-func show() -> void:
-	.show()
-	$Settings_Menu/FullscreenBox.pressed = OS.window_fullscreen
-	$UpdateSettings.start()
-	
+
 func hide() -> void:
 	.hide()
-	$UpdateSettings.stop()
+	$KeybindsPopup.call_deferred("hide")
+
+func _back_input() -> void:
+	if $KeybindsPopup.visible:
+		$KeybindsPopup.hide()
+		get_tree().set_input_as_handled()
+		return
 
 
-func _on_Back_pressed() -> void:
+func _on_Back_pressed():
 	menus.change_menu(menus.previous_menu)
-
-
-func _on_ShowSpeedrunTimerBox_toggled(button_pressed):
-	Global.speedrun_timer.visible = button_pressed
-
-
-func _on_ShowDebugBox_toggled(button_pressed):
-	Global.debug.visible = button_pressed
-
-
-func _on_AimWithMouseCheckbox_toggled(button_pressed):
-	Global.aim_with_mouse_rock_climbing = button_pressed
-
-
-func _on_FullscreenCheckbox_toggled(button_pressed):
-	OS.window_fullscreen = button_pressed
-
-
-func _on_BorderlessBox_toggled(button_pressed):
-	OS.window_borderless = button_pressed
-
-
-func _on_HideJumpArrowBox_toggled(button_pressed):
-	Global.emit_signal("hide_jump_arrow_toggled", button_pressed)
-
-
-func _on_FasterJumpChargingBox_toggled(button_pressed):
-	Global.emit_signal("faster_jump_charging_toggled", button_pressed)
-
-
-func _on_ResetBindsButton_pressed():
-	InputHelper.reset_all_actions()
-
-
-func _on_UpdateSettings_timeout():
-	$Settings_Menu/FullscreenBox.pressed = OS.window_fullscreen
