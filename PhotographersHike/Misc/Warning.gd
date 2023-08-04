41f1c89be939bc4369920304d03c69d5bc33f9fc
0megq <nvatanshenas55@gmail.com>
2023-08-03 17:16:08 -0700

    Made warning show by default

--- PhotographersHike/Misc/Warning.gd
+++ PhotographersHike/Misc/Warning.gd
@@ -1,6 +1,10 @@
 extends Label
 
 	
+func _ready() -> void:
+	show()
+	
+	
 func display_warning() -> void:
 	$AnimationPlayer.play("Show Pan Warning")
 	yield($AnimationPlayer, "animation_finished")
