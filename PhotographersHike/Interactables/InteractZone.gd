30ad42f5fce33d3b818f97bae651699b693bb54f
0megq <nvatanshenas55@gmail.com>
2023-08-09 20:21:37 -0700

    Made keyboard interact input show by default

--- PhotographersHike/Interactables/InteractZone.gd
+++ PhotographersHike/Interactables/InteractZone.gd
@@ -8,9 +8,8 @@ signal player_interacted
 
 func _ready() -> void:
 	$InteractInput.hide()
-	$InteractInput.text = InputHelper.get_action_key("interact")
+	set_interact_input_label()
 	Global.connect("input_changed", self, "set_interact_input_label")
-
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and player_in_range and !Global.dialog_box.dialog_playing:
@@ -18,7 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func set_interact_input_label() -> void:
-	if InputHelper.device == InputHelper.DEVICE_KEYBOARD:
+	if InputHelper.device == InputHelper.DEVICE_KEYBOARD or InputHelper.device == InputHelper.DEVICE_GENERIC:
 		$InteractInput.text = InputHelper.get_action_key("interact")
 	else:
 		$InteractInput.text = "Button" + str(InputHelper.get_action_button("interact"))
