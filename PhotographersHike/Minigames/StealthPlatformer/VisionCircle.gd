630102b90ffb11c6f04103bab2c50de098ee4956
0megq <nvatanshenas55@gmail.com>
2023-08-09 17:05:12 -0700

    New and improved vision circle

---
+++ PhotographersHike/Minigames/StealthPlatformer/VisionCircle.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var showing: bool = false
+
+func _ready() -> void:
+	Global.vision_circle = self
+
+
+func _physics_process(delta: float) -> void:
+	if showing:
+		$TextureRect.rect_position = Global.platforming_player.global_position - Global.player_camera.global_position + Vector2(96, 0)
+
+
+func fade_in() -> void:
+	showing = true
+	$AnimationPlayer.play("Fade In")
+	
+func fade_out() -> void:
+	$AnimationPlayer.play_backwards("Fade In")
+	yield($AnimationPlayer,"animation_finished")
+	showing = false
