85f82fa8f959afe9162e8e7272eff0fec1e0d386
0megq <nvatanshenas55@gmail.com>
2023-08-21 22:53:57 -0700

    Player can't move when in end credits

--- PhotographersHike/EndCredits.gd
+++ PhotographersHike/EndCredits.gd
@@ -9,5 +9,7 @@ func _on_SummitSign_cutscene_finished() -> void:
 	$CreditsMenu.show()
 	$CreditsMenu.modulate = Color.transparent
 	tween.tween_property($CreditsMenu, "modulate", Color.white, 5)
+	Global.platforming_player.state = Global.platforming_player.States.IDLE
+	Global.platforming_player.in_cutscene = true
 	yield(tween, "finished")
 	$CreditsMenu.play()
