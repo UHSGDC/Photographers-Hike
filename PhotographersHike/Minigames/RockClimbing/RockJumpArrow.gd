1ef63e9baf65737a8335f54d58ebcd86db27be9b
0megq <nvatanshenas55@gmail.com>
2023-09-04 12:48:46 -0700

    rock climbing max charge sound indicator

--- PhotographersHike/Minigames/RockClimbing/RockJumpArrow.gd
+++ PhotographersHike/Minigames/RockClimbing/RockJumpArrow.gd
@@ -1,12 +1,21 @@
 extends Node2D
 
+var sound_indicator_played: bool = false
 
 func _ready() -> void:
 	reset_arrow_progress()
 
 func set_arrow_progress(value: float) -> void:
 	$ArrowProgress.value = value * 100
-	$ArrowProgress/ArrowFlash.visible = value > 0.95
+	if value > 0.95:
+		if !sound_indicator_played:
+			$MaxChargeSound.play()
+			sound_indicator_played = true
+		$ArrowProgress/ArrowFlash.visible = true
+	else:
+		$ArrowProgress/ArrowFlash.visible = false
+		sound_indicator_played = false
+		
 	
 func reset_arrow_progress() -> void:
 	$ArrowProgress.value = 0
