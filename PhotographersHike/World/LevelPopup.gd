d8434b4e36b0ec35e7dad05c4f80ba60f02eb970
0megq <nvatanshenas55@gmail.com>
2023-09-07 18:24:40 -0700

    Fixed level switching issue with music and stuff

--- PhotographersHike/World/LevelPopup.gd
+++ PhotographersHike/World/LevelPopup.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	$CanvasLayer/Popup.text = level
 
 
-func _on_LevelPopup_body_entered(body: Node) -> void:
-	if body.is_in_group("player") and Global.current_level != level:
+func _on_LevelPopup_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("player") and Global.current_level != level:
 		$CanvasLayer/Popup.display_warning()
 		Global.current_level = level
